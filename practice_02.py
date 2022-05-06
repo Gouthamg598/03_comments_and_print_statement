8830f5e4adfc23d7cb46cbf3e7a044642637ac38
Gouthamg598 <104981580+Gouthamg598@users.noreply.github.com>
2022-05-06 12:32:25 +0530

    Add files via upload

---
+++ practice_02.py
@@ -0,0 +1,74 @@
+"""
+name:goutham
+date and time:04 may 2022 wednesday
+file discription: print statement and types of comments 
+""" 
+'''to print hello world'''
+# print('hello python')
+# print('hello worl'd') # syntax error 
+# to avoid thiskind of syntax use \ (backward slash)
+# print('hello worl\'d')
+# print("hello pytho'n")
+# print('hello"python')
+
+
+
+# '''to print hello python programming course in multiple lines'''
+# print('''hello python
+# programming 
+# course''')
+
+# print('''hello  
+#    python 
+# programming course''')
+
+# print('''hello python 
+#     programming course''')
+
+# print('''hello python 
+#   programming 
+# course''')
+
+#  print of empty bracket  executes blank line
+# print('hi')
+# print()
+# print('hey')
+# print(' ')
+# print(" ")
+# print(" 2")
+# print(''' 
+#  ''')
+# print('3')
+# print(""" 
+#   """)
+# print('bye')
+
+'''to print hello world'''
+
+# print('hello world')
+# print("hello world")
+# print('''hello world''')
+# print("""hello world""")
+
+'''to print hello worl'd'''
+
+# print('hello worl\'d')
+# print("hello worl'd")
+# print('''hello worl'd''')
+# print("""hello worl'd""")
+ 
+#  """to print hello worl"d"""
+
+print('hello "world')
+print("hello \"world")
+print('''hello "world''')
+print("""hello "world""")
+
+'''to print print(hello python)'''
+
+# print('print(hello python)')
+# print("print(hello python)")
+# print('''print(hello python)''')
+# print("""print(hello python)""")
+
+
